683441fe36c408fedac33b39281f30216f1bc197
samposig <samposig@gmail.com>
2022-04-24 15:29:44 -0600

    fixed route

--- route/apiRoute.js
+++ route/apiRoute.js
@@ -1,30 +1,38 @@
 const router = require('express').Router();
-
+const fs = require("fs");
 const saveData = require('../db/db.json');
+// function saveData {
 
+//  }
 // GET request
-router.get('/notes', function (req, res) {
-    saveData
-        .retrieveNotes()
-        .then(notes => res.json(notes))
-        .catch(err => res.status(500).json(err));
-});
-
+// router.get('/notes', function (req, res) {
+//     // saveData
+//     //     .retrieveNotes()
+//     //     .then(notes => res.json(notes))
+//     //     .catch(err => res.status(500).json(err));
+// });
+router.get('/notes', (req, res) => {
+    fs.readFile("../db/db.json",(err, data)=>{
+        console.log(data)
+        console.log(JSON.parse(data))
+      res.json(JSON.parse(data))
+    })
+      
+  });
 // POST request
 router.post('/notes', (req, res) => {
-    saveData
-        .addNote(req.body)
-        .then((note) => res.json(note))
-        .catch(err => res.status(500).json(err));
+   fs.writeFile("../db/db.json", (err, data)=> {
+        res.json(JSON.parse(data))
+   })
 });
 
 // Bonus - DELETE request
-router.delete('/notes/:id', function (req, res) {
-    saveData
-        .deleteNote(req.params.id)
-        .then(() => res.json({ ok: true }))
-        .catch(err => res.status(500).json(err));
-});
+// router.delete('/notes/:id', function (req, res) {
+//     saveData
+//         .deleteNote(req.params.id)
+//         .then(() => res.json({ ok: true }))
+//         .catch(err => res.status(500).json(err));
+// });
 
 
 module.exports = router;
